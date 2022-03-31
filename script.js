fe8e8c88ea62323d8a0588b7ef049362b5010d09
MikkoK189 <mikkok11@gmail.com>
2022-03-31 22:01:08 +0300

    Creating a grid and coloring it.

---
+++ script.js
@@ -0,0 +1,34 @@
+const gridContainer = document.querySelector(".grid-container");
+
+let gridSize = 100;
+let colArray = [];
+let black = false;
+
+function createGrid(gridSize) {
+  for (let x = 0; x < gridSize; x++) {
+    let colCont = document.createElement("div");
+    colCont.classList.add("column-container");
+    colArray[x] = gridContainer.appendChild(colCont);
+    for (let y = 0; y < gridSize; y++) {
+      let gridSquare = document.createElement("div");
+      gridSquare.classList.add("box");
+      gridSquare.setAttribute("id", `${x} ${y}`);
+      gridSquare.addEventListener("mouseenter", hoverOver);
+      colArray[x].appendChild(gridSquare);
+    }
+  }
+}
+
+function hoverOver(e) {
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  console.log(e.target.id);
+  if (!black) {
+    document.getElementById(
+      e.target.id
+    ).style.backgroundColor = `#${randomColor}`;
+  } else {
+    e.target.classList.add("hover");
+  }
+}
+
+createGrid(gridSize);
